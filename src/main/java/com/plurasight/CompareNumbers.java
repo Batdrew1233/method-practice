9e75db45d885b2bbf8d49487bfcaeab7e3d82aee
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-10 10:41:10 -0400

    Second Commit

--- src/main/java/com/plurasight/CompareNumbers.java
+++ src/main/java/com/plurasight/CompareNumbers.java
@@ -2,11 +2,15 @@ package com.plurasight;
 
 public class CompareNumbers {
     public static void main(String[] args) {
+    //Declare number and call Static Method isEven
         int number;
         isEven(3);
+
+        //Call Is Positive
+        isPositive(-4);
     }
 
-
+    // Static Method Is Even
     public static int isEven(int number){
         int chosenNumber = number;
 
@@ -16,5 +20,17 @@ public class CompareNumbers {
             System.out.println("Odd");
         }
         return(chosenNumber);
+    }
+
+    // Static Method Is Positive
+    public static int isPositive(int number){
+        int chosenNumber = number;
+        if (chosenNumber > 0){
+            System.out.println("Positive");
+        }else{
+            System.out.println("Negative");
+        }
+        return(chosenNumber);
+
     }
 }
