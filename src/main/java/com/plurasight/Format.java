352cde386abc020c240a052e09381bcbc4e480bc
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-10 10:17:19 -0400

    Second Commit

--- src/main/java/com/plurasight/Format.java
+++ src/main/java/com/plurasight/Format.java
@@ -2,13 +2,16 @@ package com.plurasight;
 
 public class Format {
     public static void main(String[] args) {
+        //Get first and last name
         String firstName = "Drew";
         String lastName = "Stevenson";
+
+        //Call the static method and print
         String formatName = formatName(firstName, lastName);
         System.out.println(formatName);
-
     }
 
+    //Static Method Format Name
     public static String formatName(String firstName, String lastName){
         String fullName = lastName + ", " + firstName;
         return fullName;
