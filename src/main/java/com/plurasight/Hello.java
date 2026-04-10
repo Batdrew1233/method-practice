81fcdd60793f6cbeb3e228e6cf939551ff0af67a
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-10 09:54:49 -0400

    Second Commit

--- src/main/java/com/plurasight/Hello.java
+++ src/main/java/com/plurasight/Hello.java
@@ -4,16 +4,20 @@ public class Hello {
     public static void main(String[] args) {
         sayHello();
         sayGoodbye();
+        sayGoodMorning();
 
     // Static method Hello World.
     }
     public static void sayHello(){
         System.out.println("Hello, World!");
-
     }
     // Static Method GoodBye.
     public static void sayGoodbye(){
         System.out.println("Goodbye!");
+    }
+    // Static Method Good Morning
+    public static void sayGoodMorning(){
+        System.out.println("Good Morning!");
     }
 
 
