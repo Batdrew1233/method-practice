236b422e452f54293b6a34aeafd0a839ab106d22
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-10 09:51:32 -0400

    Initial Commit

--- src/main/java/com/plurasight/Hello.java
+++ src/main/java/com/plurasight/Hello.java
@@ -1,4 +1,15 @@
 package com.plurasight;
 
 public class Hello {
+    public static void main(String[] args) {
+        sayHello();
+
+    // Static method Hello World.
+    }
+    public static void sayHello(){
+        System.out.println("Hello, World!");
+
+    }
+
+
 }
